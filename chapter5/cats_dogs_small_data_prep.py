4583de44dc0f9e53328e30d955ab6febdf5b981e
Amogh Singhal <devamoghs@gmail.com>
2018-10-25 15:17:13 +0530

    Update cats_dogs_small_data_prep.py

--- chapter5/cats_dogs_small_data_prep.py
+++ chapter5/cats_dogs_small_data_prep.py
@@ -1,6 +1,7 @@
 import os, shutil
 
-original_dataset_dir = '/home/amogh/Downloads/kaggle_original_data'
+original_cat_dataset_dir = '/home/amogh/Downloads/catsAndDogs/PetImages/Cat'
+original_dog_dataset_dir = '/home/amogh/Downloads/catsAndDogs/PetImages/Dog'
 
 # Directory to store small dataset
 base_dir = '/home/amogh/Downloads/cats_and_dogs_small'
@@ -18,7 +19,7 @@ os.mkdir(test_dir)
 train_cats_dir = os.path.join(train_dir, 'cats')
 os.mkdir(train_cats_dir)
 train_dogs_dir = os.path.join(train_dir, 'dogs')
-os.mkdir(train_cats_dir)
+os.mkdir(train_dogs_dir)
 
 # Directory with validation cat and dogs pictures
 validation_cats_dir = os.path.join(validation_dir, 'cats')
@@ -33,44 +34,51 @@ test_dogs_dir = os.path.join(test_dir, 'dogs')
 os.mkdir(test_dogs_dir)
 
 # Copies the first 1000 cat images to train_cats_dir
-fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+fnames = ['{}.jpg'.format(i) for i in range(1000)]
 for fname in fnames:
-    src = os.path.join(original_dataset_dir, fname)
+    src = os.path.join(original_cat_dataset_dir, fname)
     dst = os.path.join(train_cats_dir, fname)
     shutil.copyfile(src, dst)
 
 # Copies the first 1000 dog images to train_dog_dir
-fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+fnames = ['{}.jpg'.format(i) for i in range(1000)]
 for fname in fnames:
-    src = os.path.join(original_dataset_dir, fname)
+    src = os.path.join(original_dog_dataset_dir, fname)
     dst = os.path.join(train_dogs_dir, fname)
     shutil.copyfile(src, dst)
 
 # Copies the next 500 cat images to validation_cats_dir
-fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+fnames = ['{}.jpg'.format(i) for i in range(1000, 1500)]
 for fname in fnames:
-    src = os.path.join(original_dataset_dir, fname)
+    src = os.path.join(original_cat_dataset_dir, fname)
     dst = os.path.join(validation_cats_dir, fname)
     shutil.copyfile(src, dst)
 
 # Copies the next 500 dog images to validation_dogs_dir
-fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+fnames = ['{}.jpg'.format(i) for i in range(1000, 1500)]
 for fname in fnames:
-    src = os.path.join(original_dataset_dir, fname)
+    src = os.path.join(original_dog_dataset_dir, fname)
     dst = os.path.join(validation_dogs_dir, fname)
     shutil.copyfile(src, dst)
 
 # Copies the next 500 cat images to test_cats_dir
-fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+fnames = ['{}.jpg'.format(i) for i in range(1500, 2000)]
 for fname in fnames:
-    src = os.path.join(original_dataset_dir, fname)
+    src = os.path.join(original_cat_dataset_dir, fname)
     dst = os.path.join(test_cats_dir, fname)
     shutil.copyfile(src, dst)
 
 # Copies the next 500 dog images to test_dogs_dir
-fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+fnames = ['{}.jpg'.format(i) for i in range(1500, 2000)]
 for fname in fnames:
-    src = os.path.join(original_dataset_dir, fname)
+    src = os.path.join(original_dog_dataset_dir, fname)
     dst = os.path.join(test_dogs_dir, fname)
     shutil.copyfile(src, dst)
 
+# Sanity check
+print('total training cat images: ', len(os.listdir(train_cats_dir)))
+print('total validation cat images: ', len(os.listdir(validation_cats_dir)))
+print('total test cat images: ', len(os.listdir(test_cats_dir)))
+print('total training dog images: ', len(os.listdir(train_dogs_dir)))
+print('total validation dog images: ', len(os.listdir(validation_dogs_dir)))
+print('total test dog images: ', len(os.listdir(test_cats_dir)))
