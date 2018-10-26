424769bea77713d2dd097abca8048197cc3124ca
Amogh Singhal <devamoghs@gmail.com>
2018-10-26 16:36:37 +0530

    Create cats_dogs_pretrained_network_no_data_augmentation.py

---
+++ chapter5/cats_dogs_pretrained_network_no_data_augmentation.py
@@ -0,0 +1,103 @@
+import os
+
+import numpy as np
+from keras import layers
+from keras import models, optimizers
+from keras.applications import VGG16
+from keras_preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
+
+base_dir = '/home/amogh/Downloads/cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+conv_base = VGG16(
+    weights='imagenet',
+    include_top=False,
+    input_shape=(150, 150, 3)
+)
+
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count, ))
+    generator = datagen.flow_from_directory(
+        directory=directory,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary'
+    )
+
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size: (i + 1) * batch_size] = features_batch
+        labels[i * batch_size: (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break
+    return features, labels
+
+
+def get_model():
+    model = models.Sequential()
+    model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(
+        optimizer=optimizers.RMSprop(lr=0.00001),
+        loss='binary_crossentropy',
+        metrics=['acc']
+    )
+    return model
+
+
+def train_model(model):
+    history = model.fit(
+        x=train_features,
+        y=train_labels,
+        epochs=30,
+        batch_size=batch_size,
+        validation_data=(validation_features, validation_labels)
+    )
+    return history
+
+
+def plot_learning_curves(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (2000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (2000, 4 * 4 * 512))
+
+model = get_model()
+history = train_model(model)
+plot_learning_curves(history)
